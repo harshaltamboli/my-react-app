0fd3f20ae60e2fd71dfab55fc71b4fb4abb46fa0
harshaltamboli <harshaltamboli429@gmail.com>
2025-01-21 20:42:22 +0530

    this prorm is use a component in app.jsx

---
+++ app.jsx
@@ -0,0 +1,11 @@
+// export default function App(){
+//     return <h1> I am a App component</h1>
+// }
+import About from "./about"
+export function App() {
+    return <div>
+        <h1> I am a App component</h1> 
+        <About  />   
+        </div>
+     
+}
