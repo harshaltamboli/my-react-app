989e58161782fbdc281bf6b989bda7ffcd615ff7
harshaltamboli <harshaltamboli429@gmail.com>
2025-01-24 15:52:24 +0530

    this program is use a App . jsx file import a header and footer files

---
+++ AppFirst.jsx
@@ -0,0 +1,19 @@
+
+import Header from "./Header";
+import Foter from "./component/Foter";
+import Container from "./component/contener";
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Container/>
+            <Foter/>
+
+           
+           
+        </div>
+
+    )
+}
+
+export default App
