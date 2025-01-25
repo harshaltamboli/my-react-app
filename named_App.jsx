7ab4e488895cd90d5cee40fbcefa964111a97ff1
harshaltamboli <harshaltamboli429@gmail.com>
2025-01-25 21:25:55 +0530

    this program is use a nemed example

---
+++ named_App.jsx
@@ -0,0 +1,17 @@
+import Header from "./HeaderFirst";
+import {Footer,First_data,Secound_data} from "./Footer";
+
+function App()
+{
+    return(
+        <div>
+            <Header/>
+            <First_data/>
+            <Secound_data/>
+            <Footer/>
+
+        </div>
+    )
+}
+export default App;
+
