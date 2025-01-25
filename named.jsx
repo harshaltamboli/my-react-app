f20856782fc85913f0ab908dba2968125cbfdfbd
harshaltamboli <harshaltamboli429@gmail.com>
2025-01-25 21:27:19 +0530

    this program is use a nemed example

---
+++ named.jsx
@@ -0,0 +1,23 @@
+export function Footer(){
+    return(
+        <>
+        <h1> Footer Section </h1>
+        </>
+    )
+}
+
+export function First_data(){
+    return(
+        <>
+        <h1> this is store a contain in first data</h1>
+        </>
+    )
+}
+
+export function Secound_data(){
+    return(
+        <>
+        <h1> this is store a contain in secound data</h1>
+        </>
+    )
+}
