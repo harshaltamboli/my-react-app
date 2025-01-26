6cffbdc1884ae7021d5dd5f8a0f5072517460cb4
harshaltamboli <harshaltamboli429@gmail.com>
2025-01-26 10:23:27 +0530

    this program use a simple first cat img project

---
+++ ToDo.jsx
@@ -0,0 +1,23 @@
+function ToDo(){
+    return(
+        <>
+        <h1> Cat Information </h1>
+        <img 
+        src="public/edgar-nKC772R_qog-unsplash.jpg"
+        alt="Anil Sidu"
+        
+        class="photo"
+       />
+       
+       <ul>
+        <li>Invite new Trafic light</li>
+        <li>Rehease a movi scene</li>
+        <li>improve the spectrum tecnolgy</li>
+
+       </ul>
+      
+        </>
+    )
+}
+
+export default ToDo
