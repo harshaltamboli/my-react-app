066e7708ac026213623ab8e9ee575ff2ffb1e958
harshaltamboli <harshaltamboli429@gmail.com>
2025-02-05 13:46:06 +0530

    this program is a use a props  in array

---
+++ Parray.jsx
@@ -0,0 +1,13 @@
+ export function Parray({Collage}){
+    // console.log(nam);
+    
+
+    
+    return(
+        <>
+         <h1> Collage names</h1>
+         <h2> {Collage}</h2>
+        
+        </>
+    )
+}
