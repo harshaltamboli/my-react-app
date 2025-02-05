77408a50355be0d3757efd84fc579e3a19a2a454
harshaltamboli <harshaltamboli429@gmail.com>
2025-02-05 14:34:44 +0530

    this program is a use a props  in default prop

---
+++ Defaultprop.jsx
@@ -0,0 +1,18 @@
+import { Defaultuser } from "./Defaultuser"
+
+
+function Defaultprop(){
+return(
+    <>
+    <h1> default propes </h1>
+    <Defaultuser name="Harshal Tamboli"/>
+    <Defaultuser name="Bhavesh Pansare "/>
+    <Defaultuser />
+    <Defaultuser name="Ganesh patil"/>
+
+
+    </>
+)
+}
+
+export default Defaultprop
