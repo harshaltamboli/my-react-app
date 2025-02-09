f18b5bc7a3998eb578ab7c9d74ba127e8db7dd97
harshaltamboli <harshaltamboli429@gmail.com>
2025-02-09 09:44:04 +0530

    this program is a use a css style in inline css

---
+++ Inline.jsx
@@ -0,0 +1,145 @@
+export function Inline() {
+    const cardstyle = {
+        border: "1px solid #cccccc3b",
+        width: "200px",
+        boxShadow: "1px 2px 3px 0px #ccccc57",
+        margin: "10px"
+
+    }
+    return (
+        <>
+            <h1 style={{ color: 'red', backgroundColor: 'yellow' }}> Inline style in React Js</h1>
+
+            <div style={{ display: 'flex',flexWrap:'wrap' }}>
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '3px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+
+                    </div>
+                </div>
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+                <div>
+                    <img style={{ width: '200px' }} src="public/harshal.jfif" alt="Harshal" />
+
+                    <div style={{ padding: '5px' }}>
+                        <h4>Harshal Tamboli </h4>
+                        <p>web Developer</p>
+                    </div>
+                </div>
+
+
+
+
+
+
+            </div>
+        </>
+    )
+}
