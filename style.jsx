d6039c352e8c54b1c2e946288c8e6b56640ffff1
harshaltamboli <harshaltamboli429@gmail.com>
2025-02-09 09:43:24 +0530

    this program is a use a css style

---
+++ style.jsx
@@ -0,0 +1,14 @@
+// import { Inline } from "./Inline"
+import { Button } from "bootstrap"
+function Store(){
+    return(
+        <>
+         {/* <Inline/> */} 
+<h1> i am a button </h1>
+       <Button/>
+      
+        </>
+    )
+}
+
+export default Store
