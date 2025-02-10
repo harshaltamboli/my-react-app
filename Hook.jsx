619c79b9baa21da64b03ace062e6af6fc52306d7
harshaltamboli <harshaltamboli429@gmail.com>
2025-02-10 12:15:18 +0530

    this program is a use a Hook basic concept in UseEffect

---
+++ Hook.jsx
@@ -0,0 +1,14 @@
+import { useEffect,useState } from "react";
+import { UseEffects } from "./UseEffect";
+
+function Hook(){
+    return(
+        <>
+
+      
+        <UseEffects/>
+        </>
+    )
+}
+
+export default Hook;
